1a181a9fafaadca108ee7e410d7b75f13bed398f
tschese <tschese.smurf@gmail.com>
2020-06-16 23:43:17 +0200

    design framework and routing to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ImagesComponent } from './pages/images/images.component';
+import { VideosComponent } from './pages/videos/videos.component';
+import { ImagesModule } from './pages/images/images.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'images', loadChildren: () => import('./pages/images/images.module').then(m => m.ImagesModule) },
+  { path: 'videos', component: VideosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
