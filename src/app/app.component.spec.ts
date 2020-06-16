4c509d7d1d296e82c73398b6e3523a1fabfaed2a
tschese <tschese.smurf@gmail.com>
2020-06-16 21:26:24 +0200

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CarWebsite'`, () => {
+  it(`should have as title 'M152-Project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CarWebsite');
+    expect(app.title).toEqual('M152-Project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('CarWebsite app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('M152-Project app is running!');
   });
 });
